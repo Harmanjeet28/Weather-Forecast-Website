97b27a408b172e468f005281f2680ea7154d94be
Harmanjeet28 <150458789+Harmanjeet28@users.noreply.github.com>
2024-07-15 12:09:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-/******************************************************************************
-***
-* WEB422 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Harmanjeet Singh Hara Student ID: 118624220 Date: 27th may 2024
-*
-*
-******************************************************************************
-**/
-
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = '35e3efab0fe4a7d5850b58d5440e6842'; 
     const locationWeather = document.getElementById('location-weather');
